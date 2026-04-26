651b80c73b51b726cf1d112849ec95ec50d635e8
Niboon Boonprakob <30637687+niboon39@users.noreply.github.com>
2026-04-26 22:10:47 +0700

    save snapthot history to csv

--- script/query_status.sh
+++ script/query_status.sh
@@ -8,9 +8,11 @@ PGDATABASE="zxsmp"
 SCHEMA="tenant_14a6a16c2c9a40b8b265f332db3d342b"
 TABLE="smp_t_stb_onlinestatus"
 
-# Log file 
+# Log file (text and csv) 
 LOG_DIR="/home/postgres/smp_monitor" 
 LOG_FILE="$LOGDIR/monitor.log"
+
+CSV_FILE="$LOG_DIR/monitor_csv.csv"
 
 ##########################
 
@@ -19,6 +21,19 @@ log() {
     local ts 
     ts=$(date '+%Y-%m-%d %H:%M:%S')
     echo "$ts | $*" | tee -a "$LOG_FILE" 
+}
+
+csv_() {
+    local TS=$1
+    local ACTIVE=$2
+    local ONLINE=$3
+    local OFFLINE=$4
+
+    if [ ! -f "$CSV_FILE" ]; then 
+        echo "timestamp,active,online,offline" > "$CSV_FILE"
+    fi
+    echo "debug offline: $OFFLINE"
+    echo "$TS,$ACTIVE,$ONLINE,$OFFLINE" >> "$CSV_FILE"
 }
 
 # Test Login 
@@ -56,4 +71,11 @@ fi
 # echo "  Online     : $ONLINE  (${RATE}%)"
 # echo "  Offline    : $OFFLINE"
 
-log "active=$ACTIVE online=$ONLINE offline=$OFFLINE rate=${RATE}%"
+# Test function log
+#log "active=$ACTIVE online=$ONLINE offline=$OFFLINE rate=${RATE}%"
+
+# Test csv 
+TS=$(date '+%Y-%m-%d %H:%M:%S')
+echo "$TS | active=$ACTIVE online=$ONLINE offline=$OFFLINE rate=$RATE%" \ | tee -a $LOG_FILE
+
+csv_ "$TS" "$ACTIVE" "$ONLINE" "$OFFLINE" 
